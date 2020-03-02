fd6427844743320ad4d096d059143eb538ea5e00
zyz <1415068436@qq.com>
2020-03-02 22:15:57 +0800

    update

--- src/test/java/Name.java
+++ src/test/java/Name.java
@@ -1,11 +1,30 @@
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class Name {
     @Test
     public void testStream(){
         System.out.println(Arrays.asList(1, 3, 5, 7).stream().filter(x -> x % 2 == 0).collect(Collectors.toList()));
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char c = sc.next().charAt(0);
+        switch (c){
+            case '1':
+            case '2':
+            case '3':
+                System.out.println("这是数字");
+                break;
+            case 'a':
+            case 'b':
+                System.out.println("这是字母");
+                break;
+            default:
+                System.out.println("啥都不是");
+        }
     }
 }
